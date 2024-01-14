8b75dc4e01dcc3f175177525f8706b7ec9b48f45
Akbar-07 <116638921+Akbar-07@users.noreply.github.com>
2024-01-14 12:44:23 +0500

    aa
    aa

--- src/components/ProductsViewed/ProductsViewed.vue
+++ src/components/ProductsViewed/ProductsViewed.vue
@@ -20,4 +20,22 @@ const { vieweds } = storeToRefs(viewedsStore);
             </ProductCart>
         </div>
     </div>
+    <div class="pagination_div">
+        <div class="pagination_div_orange">
+            <h3>ПОКАЗАТЬ ЕЩЕ</h3>
+        </div>
+        <div class="pagination_div_number">
+            <h4 style="color: #FF781F">1</h4>
+            <h4>2</h4>
+            <h4>3</h4>
+            <h4>4</h4>
+            <h4>5</h4>
+            <h4>...</h4>
+            <h4>15</h4>
+        </div>
+        <div class="pagination_div_paginations">
+            <div class="pagination_pagination"></div>
+            <div id="for_gray" class="pagination_pagination"></div>
+        </div>
+    </div>
 </template>
